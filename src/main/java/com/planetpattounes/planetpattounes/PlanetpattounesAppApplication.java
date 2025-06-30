25824077394f9f44d837fff10ffea70a08c60a72
laanatri <laanatri.ade@gmail.com>
2025-06-30 10:38:07 +0200

    init project

---
+++ src/main/java/com/planetpattounes/planetpattounes/PlanetpattounesAppApplication.java
@@ -0,0 +1,13 @@
+package com.planetpattounes.planetpattounes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetpattounesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetpattounesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planetpattounes/planetpattounes/PlanetpattounesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetpattounes.planetpattounes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetpattounesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
